81e5c06be1d48b9d2aa3b40ab98a89e0a1983173
Eni Sinanaj <enisinanaj@outlook.com>
2016-10-29 15:52:41 +0200

    Initial Commit

---
+++ microtime.tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  microtime.tracker
+//
+//  Created by Eni Sinanaj on 29/10/2016.
+//  Copyright © 2016 Eni Sinanaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
