dc660ed2d10bf9e8c1147d7c1f623efa31bb6328
Andy <sinanajani@outlook.com>
2016-11-22 00:22:19 +0100

    Updated sqlite proxy

--- microtime.tracker/SQLite.swift
+++ microtime.tracker/SQLite.swift
@@ -10,10 +10,43 @@ import Foundation
 import SQLite
 
 class SQLiteProxy {
+    let db = try! Connection("myDB")
+    var times: Table? = nil
     
     func initDB() {
-        let db = try! Connection("myDB")
         
+        if (nil == self.times) {
+            createTable()
+        }
+    }
+    
+    func createTable() {
+        let times = Table("times")
+        let id = Expression<Int64>("id")
+        let startTime = Expression<Date>("start_time")
+        let duration = Expression<Int64>("duration")
+        let info = Expression<String?>("info")
+        
+        try! db.run(times.create {t in
+            t.column(id, primaryKey: true)
+            t.column(startTime)
+            t.column(duration)
+            t.column(info)
+        })
+    }
+    
+    func insertData() {
+        if (nil == times) {
+            createTable()
+        }
+        
+        // insert time duration
+    }
+    
+    func getTimes() {
+        
+        
+        //get all times from db
     }
     
 }
