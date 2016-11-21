d7767effaea6bd67593f16947245cb2f9cd6f45e
Andy <sinanajani@outlook.com>
2016-11-21 23:48:55 +0100

    Working on sqlite, probably will delete this implementation

---
+++ microtime.tracker/SQLite.swift
@@ -0,0 +1,19 @@
+//
+//  SQLite.swift
+//  microtime.tracker
+//
+//  Created by Andy Miller on 20/11/2016.
+//  Copyright © 2016 Eni Sinanaj. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class SQLiteProxy {
+    
+    func initDB() {
+        let db = try! Connection("myDB")
+        
+    }
+    
+}
